445bc7aae1d071f0019dd79b7f9701b749026114
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-16 18:54:29 +0300

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-const { PORT = 3000 } = process.env;
+const { PORT = 3005 } = process.env;
 const bdUrl = 'mongodb://localhost:27017/mestodb';
 const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 
