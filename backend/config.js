71d6dec61fe48f2c828bc6c45ddc766f8b213299
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-19 12:06:23 +0300

    move bd url to env

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,8 @@
-const { PORT = 3001 } = process.env;
-const bdUrl = 'mongodb://localhost:27017/mestodb';
+const {
+  PORT = 3000,
+  MONGO_URL = 'mongodb://localhost:27017'
+} = process.env;
+const bdUrl = `${MONGO_URL}/mestodb`;
 // const bdUrl = 'mongodb://0.0.0.0:27017/';
 const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 
