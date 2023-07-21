f17a0bcd2fedf68cbf194caccb960d18c917d672
Katya Aslanidi <grechkakat@yandex.ru>
2023-07-21 03:29:51 +0300

    добавить краш-тест https

---
+++ backend/public/precache-manifest.5daf85cafaa1ed74d5d535e8302c4125.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "330e97bf8e1de9a7938d",
+    "url": "/static/css/main.97ea18d6.chunk.css"
+  },
+  {
+    "revision": "330e97bf8e1de9a7938d",
+    "url": "/static/js/main.330e97bf.chunk.js"
+  },
+  {
+    "revision": "fbaf5c0c6dfffe7aa160",
+    "url": "/static/js/1.fbaf5c0c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48ed062521c49034ba71cb4cbda844c2",
+    "url": "/static/media/Vector.48ed0625.svg"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "60736ceca69ee30b59a39404ad7276f8",
+    "url": "/static/media/like.60736cec.svg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "2303de8a3fdc055f41c2f486a6fa6f72",
+    "url": "/static/media/liked.2303de8a.svg"
+  },
+  {
+    "revision": "b3151053dafda036acec7dd2c0e8a101",
+    "url": "/static/media/Inter-Medium.b3151053.woff2"
+  },
+  {
+    "revision": "4e6efb2458edb8fe092a5bb098623d59",
+    "url": "/static/media/pencil.4e6efb24.svg"
+  },
+  {
+    "revision": "ac29e2eb3855161d6a7cdf22d1cb8b31",
+    "url": "/static/media/add.ac29e2eb.svg"
+  },
+  {
+    "revision": "71469f0686fe275df17bc3256de196f0",
+    "url": "/static/media/trash.71469f06.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/profile-image-edit.34c46850.svg"
+  },
+  {
+    "revision": "0c1817710a2f2bff63ddd1043f1aef44",
+    "url": "/static/media/close_icon.0c181771.svg"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "874048e529a72b7ac84473e380e02600",
+    "url": "/static/media/Inter-Medium.874048e5.woff"
+  },
+  {
+    "revision": "f2deb4ebc0a6e2e4594d4eab55f80b24",
+    "url": "/index.html"
+  }
+];
